f4b9d6b93ee551fc97f5e0253f89ba60ead33ccf
quel-st <raquelsantostavares2017@gmail.com>
2022-11-14 19:39:35 -0300

    WIP:criando rotas

---
+++ src/main/java/br/com/empresa/clientes/api/ClienteControllerAPI.java
@@ -0,0 +1,12 @@
+package br.com.empresa.clientes.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/clientes")
+public class ClienteControllerAPI {
+    
+
+    
+}
