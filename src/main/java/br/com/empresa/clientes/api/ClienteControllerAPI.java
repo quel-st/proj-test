408310ed3781d9b3e0703ab90537f7d526d50cf4
quel-st <raquelsantostavares2017@gmail.com>
2022-11-14 22:34:51 -0300

    WIP: DELETE

--- src/main/java/br/com/empresa/clientes/api/ClienteControllerAPI.java
+++ src/main/java/br/com/empresa/clientes/api/ClienteControllerAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,6 +86,17 @@ public class ClienteControllerAPI {
 
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> delete(@PathVariable long id){
+        var clienteAntigo = service.findById(id);
+
+        if(clienteAntigo.getId() == 0){
+            return ResponseEntity.notFound().build();
+        }
+
+        service.delete(id);
+        return new ResponseEntity<Cliente>(clienteAntigo, HttpStatus.OK);
+    }
     
 
 }
