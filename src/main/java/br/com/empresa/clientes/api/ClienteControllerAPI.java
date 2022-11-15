1e3b8db29c2200fc0965920fa60d0c7e37e366b7
quel-st <raquelsantostavares2017@gmail.com>
2022-11-14 21:17:58 -0300

    WIP: GET

--- src/main/java/br/com/empresa/clientes/api/ClienteControllerAPI.java
+++ src/main/java/br/com/empresa/clientes/api/ClienteControllerAPI.java
@@ -1,12 +1,61 @@
 package br.com.empresa.clientes.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.empresa.clientes.entity.Cliente;
+import br.com.empresa.clientes.service.ClienteService;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
 public class ClienteControllerAPI {
     
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll(){
+        var listaClientes = service.getAll();
+        return new ResponseEntity<>(listaClientes,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable long id){
+        var umCliente = service.findById(id); 
+
+        if(umCliente.getId() == 0){
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<Cliente>(umCliente, HttpStatus.OK);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Cliente>> findByNome(@PathVariable String nome){
+        var umNome = service.findByNome(nome);
+
+        return new ResponseEntity<List<Cliente>>(umNome, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Cliente>> findByNomeRequestParam(@RequestParam("nome") String nome){
+        var listaClientes = service.findByNome(nome);
+        return new ResponseEntity<List<Cliente>>(listaClientes, HttpStatus.OK);
+    }
+
+
+
+
+
 
     
+
 }
--- src/main/java/br/com/empresa/clientes/repository/ClienteRepository.java
+++ src/main/java/br/com/empresa/clientes/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package br.com.empresa.clientes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.empresa.clientes.entity.Cliente;
@@ -8,5 +11,5 @@ import br.com.empresa.clientes.entity.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long>{
 
-    
+    List<Cliente> findByNomeIgnoreCaseContaining(@Param("nome") String nome);
 }
