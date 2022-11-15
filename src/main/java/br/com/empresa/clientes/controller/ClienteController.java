b954667b94886bf1f0ebfc5a486cb53ac2d7bbb0
quel-st <raquelsantostavares2017@gmail.com>
2022-11-15 14:38:05 -0300

    WIP:

--- src/main/java/br/com/empresa/clientes/controller/ClienteController.java
+++ src/main/java/br/com/empresa/clientes/controller/ClienteController.java
@@ -1,11 +1,15 @@
 package br.com.empresa.clientes.controller;
 
 import java.util.HashMap;
+
+import javax.validation.Valid;
+
 // import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,13 +37,15 @@ public class ClienteController {
     @GetMapping("/novo")
     public ModelAndView novo(){
         var cliente = new Cliente();
+        HashMap<String,Object> dados = new HashMap<>();
 
+        dados.put("cliente", cliente);
 
-        return new ModelAndView("cliente/form", "listaCliente",cliente);
+        return new ModelAndView("cliente/form", dados);
     }
 
     @PostMapping(params = "form")
-    public ModelAndView save(Cliente cliente, BindingResult bindResult){
+    public ModelAndView save(@Valid Cliente cliente, BindingResult bindResult){
 
 
         if(bindResult.hasErrors()){
@@ -58,5 +64,25 @@ public class ClienteController {
 
         return new ModelAndView("redirect:/clientes");
     }
+
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") long id){
+        var umCliente = service.findById(id);
+        HashMap<String,Object> dados = new HashMap<>();
+
+        dados.put("cliente", umCliente);
+
+        return new ModelAndView("cliente/form",dados);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable("id") long id){
+        service.delete(id);
+
+        return new ModelAndView("redirect:/clientes");
+    }
+
+
 
 }
